5fe4a54526fb24b2f119130ae3cec03edb1404a8
Esteban Osen <woldryk@Woldryks-MacBook-Air.local>
2026-02-18 11:46:40 -0300

    asd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,23 @@
 export default function Footer() {
-    return (
-      <footer style={styles.footer}>
-        <div style={styles.container}>
-          <p>© {new Date().getFullYear()} La Libertad Avanza Saladillo</p>
-        </div>
-      </footer>
-    );
-  }
-  
-  const styles = {
-    footer: {
-      backgroundColor: "#0F2A79",
-      color: "white",
-      padding: "30px 0",
-      marginTop: "40px",
-    },
-    container: {
-      maxWidth: "1200px",
-      margin: "0 auto",
-      textAlign: "center",
-    },
-  };
-  
+  return (
+    <footer style={styles.footer}>
+      <div style={styles.container}>
+        <p>© {new Date().getFullYear()} La Libertad Avanza Saladillo</p>
+      </div>
+    </footer>
+  );
+}
+
+const styles = {
+  footer: {
+    backgroundColor: "#0F2A79",
+    color: "white",
+    padding: "30px 0",
+    marginTop: "40px",
+  },
+  container: {
+    maxWidth: "1200px",
+    margin: "0 auto",
+    textAlign: "center",
+  },
+};
