723f6c6774b28bee1201aa2c68b9ebecb99236f0
Woldryk <woldrykdev@gmail.com>
2026-05-09 14:42:18 -0300

    ojo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ export const dynamic = "force-dynamic";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "La Libertad Avanza Saladillo",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main style={{ minHeight: "80vh" }}>{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
